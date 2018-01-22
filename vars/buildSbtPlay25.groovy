e552184e0af5fea8e4a01dfaa0f3684f3e64efc7
Rory Stokes <rmstokes@agiledigital.com.au>
2018-01-22 17:29:06 +1100

    Don't rely on env vars

--- vars/buildSbtPlay25.groovy
+++ vars/buildSbtPlay25.groovy
@@ -10,6 +10,7 @@ def call(Map config) {
   final sbt = { cmd ->
     ansiColor('xterm') {
       dir(config.baseDir) {
+        // todo: configure these 
         sh "sbt -batch -sbt-dir /home/jenkins/.sbt -Dsbt.repository.config=/home/jenkins/sbt.boot.properties -Dsbt.ivy.home=/home/jenkins/.ivy2/ -Divy.home=/home/jenkins/.ivy2/ -v \"${cmd}\""
       }
     }
@@ -46,29 +47,28 @@ def call(Map config) {
     }
 
     stage('Inject configuration') {
+      def subPath = "${config.baseDir}/modules/api/"
       // TODO: Allow ${SETTINGS_CONTEXT} to be overriden
-      withEnv(["SUB_PATH=${config.baseDir}/modules/api/"]) {
         // From https://stash.agiledigital.com.au/projects/MCP/repos/docker-builder/browse/builders/play2-multi-build/build.sh
-        sh '''
-        |# Insert the project.conf, environment.conf, etc into the deployable.
-        |cp *.conf "${SUB_PATH}conf"
-        |
-        |# Create the conf file that ties the application.conf and environment.conf together.
-        |echo 'include "application.conf"' > "${SUB_PATH}conf/combined.conf"
-        |echo 'include "environment.conf"' >> "${SUB_PATH}conf/combined.conf"
-        |echo 'include "topology.conf"' >> "${SUB_PATH}conf/combined.conf"
-        |
-        |# Allow the application.context variable to be overridden.
-        |echo 'play.http.context=/' >> "${SUB_PATH}conf/combined.conf"
-        |echo 'play.http.context=${?APPLICATION_CONTEXT}' >> "${SUB_PATH}conf/combined.conf"
-        |
-        |# Allow cryto to be changed
-        |echo 'play.crypto.secret=${?APPLICATION_SECRET}' >> "${SUB_PATH}conf/combined.conf"
-        |
-        |cat "${SUB_PATH}conf/combined.conf"
-        |
-        |'''.stripMargin()
-      }
+      sh """
+      |# Insert the project.conf, environment.conf, etc into the deployable.
+      |cp *.conf "${subPath}conf"
+      |
+      |# Create the conf file that ties the application.conf and environment.conf together.
+      |echo 'include "application.conf"' > "${subPath}conf/combined.conf"
+      |echo 'include "environment.conf"' >> "${subPath}conf/combined.conf"
+      |echo 'include "topology.conf"' >> "${subPath}conf/combined.conf"
+      |
+      |# Allow the application.context variable to be overridden.
+      |echo 'play.http.context=/' >> "${subPath}conf/combined.conf"
+      |echo 'play.http.context=\${?APPLICATION_CONTEXT}' >> "${subPath}conf/combined.conf"
+      |
+      |# Allow cryto to be changed
+      |echo 'play.crypto.secret=\${?APPLICATION_SECRET}' >> "${subPath}conf/combined.conf"
+      |
+      |cat "${subPath}conf/combined.conf"
+      |
+      |""".stripMargin()
     }
     stage('Package') {
       sbt ";project ${config.component}; set name := \"${fullComponentName}\"; set version: \"${buildVersion}\"; dist"
