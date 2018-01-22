4b5013f2ed068736610e37e3707cbaf9d295311a
Rory Stokes <rmstokes@agiledigital.com.au>
2018-01-22 16:13:37 +1100

    Allow for optional containertemplates

--- vars/buildContainerTemplateSbtPlay25.groovy
+++ vars/buildContainerTemplateSbtPlay25.groovy
@@ -1,9 +1,11 @@
 def call() {
-	return containerTemplate(
-		name: 'build-sbt-play25',
-		image: 'agiledigital/build-image-sbt-play25',
-        alwaysPullImage: true,
-		command: 'cat',
-		ttyEnabled: true
-	)
+	return [
+		containerTemplate(
+			name: 'build-sbt-play25',
+			image: 'agiledigital/build-image-sbt-play25',
+	        alwaysPullImage: true,
+			command: 'cat',
+			ttyEnabled: true
+		)
+	]
 }
