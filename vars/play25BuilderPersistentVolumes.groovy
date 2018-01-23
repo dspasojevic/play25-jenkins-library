43ed09af970527f51d15de10680f766b5ac5a0c5
Rory Stokes <rmstokes@agiledigital.com.au>
2018-01-23 17:33:59 +1100

    Rename name to claimName

--- vars/play25BuilderPersistentVolumes.groovy
+++ vars/play25BuilderPersistentVolumes.groovy
@@ -2,12 +2,12 @@ def call(Map config) {
   return [
     [
       path: '/home/jenkins/.ivy2',
-      name: "${config.project}-home-jenkins-ivy2",
+      claimName: "${config.project}-home-jenkins-ivy2",
       sizeGiB: 2
     ],
     [
       path: '/home/jenkins/.sbt',
-      name: "${config.project}-home-jenkins-sbt",
+      claimName: "${config.project}-home-jenkins-sbt",
       sizeGiB: 2
     ]
   ]
