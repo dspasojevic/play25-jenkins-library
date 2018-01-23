8edd5089c9397872e6fc68fc5b6ce6a983532dc5
Rory Stokes <rmstokes@agiledigital.com.au>
2018-01-23 16:33:15 +1100

    Handle multiple stages, rework volumes

--- vars/buildPersistentVolumesSbtPlay25.groovy
+++ vars/buildPersistentVolumesSbtPlay25.groovy
@@ -1,6 +1,9 @@
-def call() {
-	return [
-		'/home/jenkins/.ivy2',
-		'/home/jenkins/.sbt'
-	] 
+def call(Map config) {
+  return [
+    [path: '/home/jenkins/.ivy2', sizeGiB: 2],
+    [path: '/home/jenkins/.sbt',  sizeGiB: 2]
+  ].collect { volume ->
+    volume.name = "${config.project}-${path.replaceAll(/[^a-zA-Z0-9]+/, '-')}"
+    volume
+  }
 }
--- vars/buildSbtPlay25.groovy
+++ vars/buildSbtPlay25.groovy
@@ -46,40 +46,44 @@ def call(Map config) {
       sbt ";project ${config.component}; testOnly ** -- junitxml console"
       junit "${config.baseDir}/modules/**/target/test-reports/**/*.xml"
     }
-
-
-    stage('Inject configuration') {
-      // TODO: Allow ${SETTINGS_CONTEXT} to be overriden
-        // From https://stash.agiledigital.com.au/projects/MCP/repos/docker-builder/browse/builders/play2-multi-build/build.sh
-      sh """
-      |# Insert the project.conf, environment.conf, etc into the deployable.
-      |cp *.conf "${modulePath}/conf"
-      |
-      |# Create the conf file that ties the application.conf and environment.conf together.
-      |echo 'include "application.conf"' > "${modulePath}/conf/combined.conf"
-      |echo 'include "environment.conf"' >> "${modulePath}/conf/combined.conf"
-      |echo 'include "topology.conf"' >> "${modulePath}/conf/combined.conf"
-      |
-      |# Allow the application.context variable to be overridden.
-      |echo 'play.http.context=/' >> "${modulePath}/conf/combined.conf"
-      |echo 'play.http.context=\${?APPLICATION_CONTEXT}' >> "${modulePath}/conf/combined.conf"
-      |
-      |# Allow cryto to be changed
-      |echo 'play.crypto.secret=\${?APPLICATION_SECRET}' >> "${modulePath}/conf/combined.conf"
-      |
-      |cat "${modulePath}/conf/combined.conf"
-      |
-      |""".stripMargin()
-    }
-    stage('Package') {
-      sbt ";project ${config.component}; set name := \"${fullComponentName}\"; set version := \"${buildVersion}\"; dist"
-    }
   }
 
-  stage('Archive to Jenkins') {
-    def tarName = "${fullComponentName}-${buildVersion}.tar.gz"
-    // Re-compress dist zip file as tar gzip without top level folder
-    sh "unzip ${modulePath}/target/universal/${fullComponentName}-${buildVersion}.zip"
-    sh "tar -czvf \"${tarName}\" -C \"${fullComponentName}-${buildVersion}\" ."
+  if(stage == 'dist') { 
+    container('build-sbt-play25') {
+      stage('Inject configuration') {
+        // TODO: Allow ${SETTINGS_CONTEXT} to be overriden
+          // From https://stash.agiledigital.com.au/projects/MCP/repos/docker-builder/browse/builders/play2-multi-build/build.sh
+        sh """
+        |# Insert the project.conf, environment.conf, etc into the deployable.
+        |cp *.conf "${modulePath}/conf"
+        |
+        |# Create the conf file that ties the application.conf and environment.conf together.
+        |echo 'include "application.conf"' > "${modulePath}/conf/combined.conf"
+        |echo 'include "environment.conf"' >> "${modulePath}/conf/combined.conf"
+        |echo 'include "topology.conf"' >> "${modulePath}/conf/combined.conf"
+        |
+        |# Allow the application.context variable to be overridden.
+        |echo 'play.http.context=/' >> "${modulePath}/conf/combined.conf"
+        |echo 'play.http.context=\${?APPLICATION_CONTEXT}' >> "${modulePath}/conf/combined.conf"
+        |
+        |# Allow cryto to be changed
+        |echo 'play.crypto.secret=\${?APPLICATION_SECRET}' >> "${modulePath}/conf/combined.conf"
+        |
+        |cat "${modulePath}/conf/combined.conf"
+        |
+        |""".stripMargin()
+      }
+      stage('Package') {
+        sbt ";project ${config.component}; set name := \"${fullComponentName}\"; set version := \"${buildVersion}\"; dist"
+      }
+    }
+
+    stage('Archive to Jenkins') {
+      def tarName = "${fullComponentName}-${buildVersion}.tar.gz"
+      // Re-compress dist zip file as tar gzip without top level folder
+      sh "unzip ${modulePath}/target/universal/${fullComponentName}-${buildVersion}.zip"
+      sh "tar -czvf \"${tarName}\" -C \"${fullComponentName}-${buildVersion}\" ."
+      archiveArtifacts tarName
+    }
   }
 }
