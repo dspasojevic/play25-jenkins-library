31001e8b2e860f27644ff8615d30bb878f848521
Rory Stokes <rmstokes@agiledigital.com.au>
2018-01-23 16:48:14 +1100

    Rename pipeline methods

---
+++ vars/play25BuilderPersistentVolumes.groovy
@@ -0,0 +1,9 @@
+def call(Map config) {
+  return [
+    [path: '/home/jenkins/.ivy2', sizeGiB: 2],
+    [path: '/home/jenkins/.sbt',  sizeGiB: 2]
+  ].collect { volume ->
+    volume.name = "${config.project}-${volume.path.replaceAll(/[^a-zA-Z0-9]+/, '-')}"
+    volume
+  }
+}
